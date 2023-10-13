6d6eb125d09beeeb66c8ee2a09cbd601997d4262
rsaha <rah4927@gmail.com>
2023-10-13 01:53:10 -0400

    commit refactored server

--- python/server.py
+++ python/server.py
@@ -5,6 +5,9 @@ import transformers
 import torch
 import os
 import time
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+from pyngrok import ngrok
 
 
 parser = argparse.ArgumentParser()
@@ -13,6 +16,10 @@ parser.add_argument('--temperatures', type=float, nargs='+', default=[0.5])
 parser.add_argument('--num-samples', type=int, default=10)
 args = parser.parse_args()
 
+# Prompt template for the default model.
+# Change this if your model expects a different input format.
+def llmstep_prompt(tactic_state, prefix):
+  return '[GOAL]%s[PROOFSTEP]%s' % (tactic_state, prefix)
 
 def load_hf(hf_model):
     print("Loading model...")
@@ -29,13 +36,20 @@ def load_hf(hf_model):
     return model, tokenizer
 
 
-def generate(prompt, temperatures, num_samples):
+def hf_generate(
+    model,
+    tokenizer,
+    prompt,
+    temperatures,
+    num_samples,
+    max_new_tokens=128
+):
     input_ids = tokenizer.encode(prompt, return_tensors='pt').to(model.device)
     texts = []
     for temp in temperatures:
         out = model.generate(
             input_ids,
-            max_new_tokens=128,
+            max_new_tokens=max_new_tokens,
             do_sample=temp > 0,
             temperature=temp,
             pad_token_id=tokenizer.eos_token_id,
@@ -50,27 +64,78 @@ def generate(prompt, temperatures, num_samples):
     return texts
 
 
-model, tokenizer = load_hf(args.hf_model)
-app = Flask(__name__)
+class LLMStepServer(HTTPServer):
+    def __init__(
+        self, model, tokenizer, generate_function, config, *args, **kwargs
+    ):
+      self.model = model
+      self.tokenizer = tokenizer
+      self.generate_function = generate_function
+      self.config = config
+      super().__init__(*args, **kwargs)
 
-@app.route('/', methods=['POST'])
-def process_request():
-    start = time.time()
-    data = request.get_json()
 
-    tactic_state = data.get('tactic_state')
-    prefix = data.get('prefix')
+class LLMStepRequestHandler(BaseHTTPRequestHandler):
+    def process_request(self, tactic_state, prefix):
+        prompt = self.server.config['LLMSTEP_PROMPT'](tactic_state, prefix)
+        texts = self.server.generate_function(
+            model=self.server.model,
+            tokenizer=self.server.tokenizer,
+            prompt=prompt,
+            temperatures=self.server.config['LLMSTEP_TEMPERATURES'],
+            num_samples=self.server.config['LLMSTEP_NUM_SAMPLES']
+        )
+        texts = [prefix + text for text in texts]
+        response = {"suggestions": texts}
+        return response
 
-    prompt = """[GOAL]%s[PROOFSTEP]%s""" % (tactic_state, prefix)
-    texts = generate(prompt, args.temperatures, args.num_samples)
-    texts = [prefix + text for text in texts]
+    def do_POST(self):
+        # Set response headers
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
 
-    response = {"suggestions": texts}
-    end = time.time()
-    print("%d suggestions (%.3fs)" % (len(texts), (end-start)))
-    return jsonify(response)
+        # Get the incoming POST data
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length).decode('utf-8')
+
+        try:
+            data = json.loads(post_data)
+            result = self.process_request(data['tactic_state'], data['prefix'])
+            response = result
+            self.wfile.write(json.dumps(response).encode('utf-8'))
+        except Exception as e:
+            # Handle errors gracefully
+            error_response = {'error': str(e)}
+            self.wfile.write(json.dumps(error_response).encode('utf-8'))
+
+
 
 
 if __name__ == '__main__':
-    port = os.environ.get('LLMSTEP_PORT', 5000)
-    app.run(use_reloader=False, host='0.0.0.0', port=port)
+  # Download and load the model (this takes a few minutes).
+  model, tokenizer = load_hf(args.hf_model)
+  host = os.environ.get('LLMSTEP_HOST', 'localhost')
+  port = os.environ.get('LLMSTEP_PORT', 5000)
+
+  config = {
+    'LLMSTEP_MODEL': args.hf_model,
+
+    # Sampling temperature(s)
+    'LLMSTEP_TEMPERATURES': args.temperatures,
+
+    # Number of generated suggestions per temperature
+    'LLMSTEP_NUM_SAMPLES': args.num_samples,
+
+    # Prompt template
+    'LLMSTEP_PROMPT': llmstep_prompt
+}
+  # Create the server
+  server_address = (host, port)
+  httpd = LLMStepServer(
+    model, tokenizer, hf_generate, config,
+    server_address, LLMStepRequestHandler
+  )
+
+  print('Server started')
+  httpd.serve_forever()
--- python/suggest.py
+++ python/suggest.py
@@ -10,19 +10,7 @@ PORT = os.environ.get('LLMSTEP_PORT', 5000)
 SERVER = os.environ.get('LLMSTEP_SERVER', 'DEFAULT')
 
 
-def suggest(host, port, tactic_state, prefix):
-    conn = http.client.HTTPConnection(host, port)
-    headers = {'Content-type': 'application/json'}
-    body = json.dumps({"tactic_state": tactic_state, "prefix": prefix})
-    conn.request("POST", "/", body, headers)
-    response = conn.getresponse()
-    data = response.read()
-    data_dict = json.loads(data)
-    print('[SUGGESTION]'.join(data_dict['suggestions']))
-    conn.close()
-
-
-def suggest_colab(host, tactic_state, prefix):
+def suggest(host, tactic_state, prefix):
     data = {'tactic_state': tactic_state, 'prefix': prefix}
     response = json.loads(requests.post(host, json=data).content)
     print('[SUGGESTION]'.join(response['suggestions']))
@@ -30,6 +18,8 @@ def suggest_colab(host, tactic_state, prefix):
 
 if __name__ == "__main__":
     if SERVER == 'COLAB':
-        suggest_colab(HOST, sys.argv[1], sys.argv[2])
+        URL = HOST
     else:
-        suggest(HOST, PORT, sys.argv[1], sys.argv[2])
+        URL = f'http://{HOST}/{PORT}'
+
+    suggest(URL, sys.argv[1], sys.argv[2])
